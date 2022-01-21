83bec4c8491b716dd532b030ea36b440973ef8bd
Ronald Seah <ronseahald@hotmail.com>
2022-01-21 19:27:10 +0800

    Remove unused function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,4 @@
-use std::env;
-
 mod cli;
-
-fn get_filename() -> String {
-    let args: Vec<String> = env::args().collect();
-    let filename_str = args.get(1);
-    match filename_str {
-        Some(filename) => String::from(filename),
-        None => String::new(),
-    };
-    return String::from(&args[1]);
-}
 
 
 fn main() {
