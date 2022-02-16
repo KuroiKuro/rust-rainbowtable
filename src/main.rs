e70e28e89dafc96630c7d1224a93e42faa43c558
Ronald Seah <ronseahald@hotmail.com>
2022-02-16 19:26:59 +0800

    Remove debug print code

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,5 @@ mod operations;
 
 fn main() {
     let program_options = cli::parse_cli();
-    println!("Program options:");
-    println!("Operation: {}", program_options.operation);
     operations::select_run(program_options);
 }
